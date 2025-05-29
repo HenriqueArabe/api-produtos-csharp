ce5c5069e8f587f8a4e9b71900f49543758ea9ec
Henrique Arabe <riqueneres01020304@gmail.com>
2025-05-29 14:01:35 -0300

    Ajuste Program.cs para configurar SwaggerUI na raiz e redirecionamento HTTPS

--- API_Produtos/Models/Produtos.cs
+++ API_Produtos/Models/Produtos.cs
@@ -1,9 +1,16 @@
-﻿namespace API_Produtos.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API_Produtos.Models
 {
     public class Produto
     {
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 3)]
         public string Nome { get; set; } = string.Empty;
+
+        [Range(0.01, 10000, ErrorMessage = "O preço deve estar entre 0.01 e 10000.")]
         public decimal Preco { get; set; }
     }
 }
--- API_Produtos/Program.cs
+++ API_Produtos/Program.cs
@@ -1,19 +1,26 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services
+    .AddControllers()
+    .ConfigureApiBehaviorOptions(options =>
+    {
+        options.SuppressModelStateInvalidFilter = false;
+    });
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        // Caminho raiz: https://localhost:
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "API Produtos v1");
+        c.RoutePrefix = string.Empty;
+    });
 }
 
 app.UseAuthorization();
@@ -21,4 +28,3 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
-// Projeto
