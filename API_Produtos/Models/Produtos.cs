68e4b296f88dd47a75136ca84bee920f5a0b533f
Henrique Arabe <riqueneres01020304@gmail.com>
2025-05-29 14:02:57 -0300

    Add DataAnnotations ao model Produto para validações

--- API_Produtos/Models/Produtos.cs
+++ API_Produtos/Models/Produtos.cs
@@ -6,11 +6,13 @@ namespace API_Produtos.Models
     {
         public int Id { get; set; }
 
-        [Required]
-        [StringLength(100, MinimumLength = 3)]
+        [Required(ErrorMessage = "O nome é obrigatório.")]
+        [StringLength(100, MinimumLength = 3,
+            ErrorMessage = "Nome deve ter entre 3 e 100 caracteres.")]
         public string Nome { get; set; } = string.Empty;
 
-        [Range(0.01, 10000, ErrorMessage = "O preço deve estar entre 0.01 e 10000.")]
+        [Range(0.01, 10000,
+            ErrorMessage = "Preço deve estar entre R$0,01 e R$10.000,00.")]
         public decimal Preco { get; set; }
     }
 }
